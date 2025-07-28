62fc20c1e8e912cbb698cf1d56b498783209ff13
unknown <tahsinabdulla107@gmail.com>
2025-07-28 16:11:45 +0600

    New build assets

--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,17 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 manualChunks: undefined,
+                // Better cache busting with timestamp
+                assetFileNames: (assetInfo) => {
+                    const info = assetInfo.name.split('.');
+                    const ext = info[info.length - 1];
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+                        return `assets/[name]-[hash][extname]`;
+                    }
+                    return `assets/[name]-[hash].${ext}`;
+                },
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js',
             },
         },
     },
